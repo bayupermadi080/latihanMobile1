cafbad5db30d7e6f649a8c2aef349b23f54adecb
bayupermadi080 <bayu.nexus@gmail.com>
2017-04-17 22:46:31 +0800

    coba upload

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import MainApp from './src/MainApp';
+AppRegistry.registerComponent('ch2p1', () => MainApp);
